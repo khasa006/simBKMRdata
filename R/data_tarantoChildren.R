12496363a72dc74f129b927a5a84eccb5b8a83f8
Kazi Tanvir Hasan <khasa006@fiu.edu>
2025-04-04 09:53:00 -0400

    update unite test; update vignettes; need to fixed some error

--- R/data_tarantoChildren.R
+++ R/data_tarantoChildren.R
@@ -1,26 +1,26 @@
 #' Taranto Children's Environmental Exposure Data
-#' 
+#'
 #' @description
 #' A short description...
-#' 
+#'
 #' @format ## `who`
 #' A data frame with 437 rows and 13 columns:
 #' \describe{
-#'   \item{ID}{}
-#'   \item{age}{}
-#'   \item{QI}{}
-#'   \item{Cadmium}{}
-#'   \item{Mercury}{}
-#'   \item{Arsenic}{}
-#'   \item{Lead}{}
-#'   \item{Manganese}{}
-#'   \item{Sex}{}
-#'   \item{BMI}{}
-#'   \item{SES}{}
-#'   \item{Distance_metres}{}
-#'   \item{SPM}{}
+#'   \item{ID}{Subject's ID}
+#'   \item{age}{Subject's age in years}
+#'   \item{QI}{Intelligence quotient}
+#'   \item{Cadmium}{Cd urine concentration	ng/ml}
+#'   \item{Mercury}{Hg urine concentration	ng/ml}
+#'   \item{Arsenic}{As urine concentration	ng/ml}
+#'   \item{Lead}{Pb blood concentration	ng/ml}
+#'   \item{Manganese}{Mn hair concentration	ng/g}
+#'   \item{Sex}{Student's gender; 1 = Male, 2 = Female}
+#'   \item{BMI}{Body Mass Index (kg/m2)}
+#'   \item{SES}{Social Economic Status; 1 = LOW, 2 = MEDIUM", 3 = HIGH}
+#'   \item{Distance_metres}{Distance to nearest industrial site (m)}
+#'   \item{SPM}{Raven's Standard Progressive Matrices}
 #' }
-#' 
+#'
 #' @source Prof. Roberto Lucchini; see inst/scripts/data_tarantoChildren*.R for
 #' more details. Licensed under CC BY 4.0.
 "tarantoChildren_df"
